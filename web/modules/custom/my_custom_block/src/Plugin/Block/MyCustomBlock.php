47d95037ff778bc06ce52babd7d150886a81a631
ishakzail <ishak.zail.iz@gmail.com>
2024-02-25 18:06:42 +0100

    [FEAT]:  add func to generate random quote from a string of quotes

--- web/modules/custom/my_custom_block/src/Plugin/Block/MyCustomBlock.php
+++ web/modules/custom/my_custom_block/src/Plugin/Block/MyCustomBlock.php
@@ -1,27 +1,83 @@
 <?php
 
 namespace Drupal\my_custom_block\Plugin\Block;
-
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
- * Provides a 'My Custom Block' block.
- *
- * @Block(
- *   id = "my_custom_block",
- *   admin_label = @Translation("My Custom Block"),
- * )
- */
+* Provides a 'My Custom Block' Block.
+*
+* @Block(
+* id = "my_custom_block",
+* admin_label = @Translation("My Custom Block"),
+* category = @Translation("Custom"),
+* )
+*/
 class MyCustomBlock extends BlockBase {
 
   /**
-   * {@inheritdoc}
-   */
-  public function build() {
+  * {@inheritdoc}
+  */
+  public function defaultConfiguration() {
     return [
-      '#markup' => $this->t('Hello, World! This is my custom block.'),
+      'block_content' => '',
+    ] + parent::defaultConfiguration();
+  }
+
+  /**
+  * {@inheritdoc}
+  */
+  public function blockForm($form, FormStateInterface $form_state) {
+    $form['block_content'] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Block content'),
+      '#description' => $this->t('The content to display in the block.'),
+      '#default_value' => $this->configuration['block_content'],
+    ];
+    return $form;
+  }
+
+  /**
+  * {@inheritdoc}
+  */
+  public function blockSubmit($form, FormStateInterface $form_state) {
+    $this->configuration['block_content'] = $form_state->getValue('block_content');
+  }
+
+
+
+  /**
+  * {@inheritdoc}
+  */
+  public function build() {
+    
+    // Generate a random color.
+    $color = '#' . substr(md5(mt_rand()), 0, 6);
+    // Generate random quote from the data entred by the user
+    // block_content contains the quotes separated by a comma
+    $content = $this->extractRandomWord($this->configuration['block_content']);
+
+    return [
+      '#markup' => '<strong>' .$content . '</strong>',
+      '#attributes' => [
+        'style' => 'color :'. $color
+      ],
+      '#cache' => [
+        'max-age' => 0,
+      ],
     ];
   }
+
+  private function extractRandomWord($data)
+  {
+    // split the string and store it into an array
+    $words = explode(",", $data);
+    // get the key of the random value from the array
+    $rand_key = array_rand($words);
+    // return the string using it key
+    return $words[$rand_key];
+  }
 }
+
 
 
